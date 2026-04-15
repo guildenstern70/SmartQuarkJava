cd8423c3034b9f83da59dab3368855766013daea
Alessio Saltarin <alessiosaltarin@gmail.com>
2026-04-15 16:19:31 +0200

    Added Home Page

---
+++ src/main/java/net/littlelite/controller/web/IndexController.java
@@ -0,0 +1,30 @@
+/*
+ * The SmartQuark Project Java Edition
+ * Copyright (c) Alessio Saltarin, 2026
+ * This software is licensed under ISC License
+ * See LICENSE
+ */
+
+package net.littlelite.controller.web;
+
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/")
+public class IndexController
+{
+    @Inject
+    Template index;
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public TemplateInstance get()
+    {
+        return index.instance();
+    }
+}
